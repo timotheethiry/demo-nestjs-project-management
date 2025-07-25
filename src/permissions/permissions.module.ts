28d053da993b2d7555f787b493a46f1f53c7a74e
Timothée Thiry <timothee.thiry@gmail.com>
2025-07-25 11:22:30 +0200

    Permissions module and service

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PermissionsService } from './permissions.service';
 
-@Module({})
+@Module({
+	exports: [PermissionsService],
+	providers: [PermissionsService],
+})
 export class PermissionsModule {}
---
+++ src/permissions/permissions.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@nestjs/common';
+import { Project } from 'src/project/entities/project.entity';
+import { Role } from 'src/shared/role.enum';
+import { JwtPayload } from 'src/shared/types/jwt-payload.interface';
+import { User } from 'src/user/entities/user.entity';
+
+/**
+ *
+ * @param currentUser - Utilisateur authentifié (généralement currentUser)
+ * @param target - Ressource User à comparer
+ * @param project - Projet concerné
+ *
+ */
+
+@Injectable()
+export class PermissionsService {
+	isAdmin(currentUser: JwtPayload): boolean {
+		return currentUser.role === Role.Admin;
+	}
+
+	isOwner(currentUser: JwtPayload, target: User): boolean {
+		return currentUser.sub === target.id;
+	}
+
+	isProjectOverseer(currentUser: JwtPayload, project: Project): boolean {
+		return project.overseer?.id === currentUser.sub;
+	}
+
+	isProjectMember(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			project.members?.some((member) => member.id === currentUser.sub) ?? false
+		);
+	}
+
+	canViewProject(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) ||
+			this.isProjectOverseer(currentUser, project) ||
+			this.isProjectMember(currentUser, project)
+		);
+	}
+
+	canEditPhase(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) ||
+			this.isProjectOverseer(currentUser, project) ||
+			this.isProjectMember(currentUser, project)
+		);
+	}
+
+	canDeletePhase(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) || this.isProjectOverseer(currentUser, project)
+		);
+	}
+
+	canEditTask(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) ||
+			this.isProjectOverseer(currentUser, project) ||
+			this.isProjectMember(currentUser, project)
+		);
+	}
+
+	canDeleteTask(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) || this.isProjectOverseer(currentUser, project)
+		);
+	}
+
+	canProposeProjectClosure(currentUser: JwtPayload, project: Project): boolean {
+		return this.isProjectOverseer(currentUser, project);
+	}
+
+	canCloseProject(currentUser: JwtPayload): boolean {
+		return this.isAdmin(currentUser);
+	}
+
+	canAssignOverseer(currentUser: JwtPayload): boolean {
+		return this.isAdmin(currentUser);
+	}
+
+	canAssignMember(currentUser: JwtPayload, project: Project): boolean {
+		return (
+			this.isAdmin(currentUser) || this.isProjectOverseer(currentUser, project)
+		);
+	}
+}
